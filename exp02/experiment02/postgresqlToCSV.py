8d1c18ad54fa8880635d9f90ac526304f50d10de
Kelvin Likollari <likolk@usi.ch>
2023-12-05 13:43:08 +0100

    fix

--- exp02/experiment02/postgresqlToCSV.py
+++ exp02/experiment02/postgresqlToCSV.py
@@ -1,37 +1,29 @@
-# this file will be used to export the postgresql table onto a csv file, as required.
-import psycopg2, csv
-# from dotenv import load_dotenv
-# import os
+import psycopg2
 
-# load_dotenv()
+try:
+    conn = psycopg2.connect(
+        host="dpg-cljgru6g1b2c73apkn90-a.frankfurt-postgres.render.com",
+        database="exp02",
+        user="kelvin",
+        password="8r9aws8VsQyRxv3a9vcqB9PFcA6VUKYz",
+        port="5432"
+    )
 
-# connect to the database
-conn = psycopg2.connect(
-    host="dpg-cljgru6g1b2c73apkn90-a.frankfurt-postgres.render.com",
-    database= "exp02",
-    user= "kelvin",
-    password="8r9aws8VsQyRxv3a9vcqB9PFcA6VUKYz",
-    port="5432"
-)
+    cursor = conn.cursor()
 
-# create a cursor
-cursor = conn.cursor()
+    cursor.execute("SELECT * FROM myapp_demographics")
 
-# fetch all the table content
-cursor.execute("SELECT * FROM myapp_demographics")
+    with open('demographics.csv', 'w') as f:
+        for row in cursor:
+            f.write("%s\n" % str(row))
 
-# store into demographics.csv file
-with open('demographics.csv', 'w') as f:
-    for row in cursor:
-        f.write("%s\n" % str(row))
+    print("CSV file created/updated successfully!")
 
-# close connection and cursor
-cursor.close()
-conn.close()
+except psycopg2.Error as e:
+    print("Error occurred while connecting to PostgreSQL:", e)
 
-
-
-
-
-
-
+finally:
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
